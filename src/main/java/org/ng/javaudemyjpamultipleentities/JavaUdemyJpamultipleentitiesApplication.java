b00a737550fdc001d1644308971c9e9bb2c1ca5f
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-10-14 11:16:55 +0200

    init commit

---
+++ src/main/java/org/ng/javaudemyjpamultipleentities/JavaUdemyJpamultipleentitiesApplication.java
@@ -0,0 +1,13 @@
+package org.ng.javaudemyjpamultipleentities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUdemyJpamultipleentitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUdemyJpamultipleentitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ng/javaudemyjpamultipleentities/JavaUdemyJpamultipleentitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ng.javaudemyjpamultipleentities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUdemyJpamultipleentitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
